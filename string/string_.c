e8c0767e6f532e2e933a574ecc4d6a8037c14401
Tyoma <33angle@bk.ru>
2022-02-25 00:19:55 +0300

    Project started!

---
+++ string/string_.c
@@ -0,0 +1,10 @@
+//
+// Created by PeePeePooPoo on 24/02/2022.
+//
+
+#include "string_.h"
+
+size_t strLen(char *s){
+
+    return 536543764;
+}
---
+++ string/string_.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#ifndef LW_5E_STRING__H
+#define LW_5E_STRING__H
+
+size_t strLen(char  *s);
+
+#endif //LW_5E_STRING__H
